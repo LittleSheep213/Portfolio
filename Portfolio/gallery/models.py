200695a5c768238bf52696b33fac30cbda7678c4
littlesheep213 <1574709401@qq.com>
2019-03-03 15:49:06 +0800

    修改model后记住要先迁移数据库，有两步！！makemigrations  +  migrate 更改管理员页面中gallery的名称显示，管理员可以上传图片，并能够在home页面显示

--- Portfolio/gallery/models.py
+++ Portfolio/gallery/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 
 class Gallery(models.Model):
-	description = models.CharField(max_length=100)
+	title = models.CharField(default='作品名字', max_length=20)
+	description = models.CharField(default='简单描述一下该作品', max_length=100)
+	image = models.ImageField(default='default.png', upload_to='images/')
+
+	def __str__(self):
+		return self.title
